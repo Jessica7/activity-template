849590e831f9ed3bbd58a5ff655be7f3f7a0002f
Andre Alves Garzia <contato@andregarzia.com>
2015-07-29 18:23:16 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,104 @@
+var data = {
+    config: {},
+    activity: {}
+};
+
+
+function bindNavigationData() {
+    var source   = $("#navigation-template").html();
+    var template = Handlebars.compile(source);
+
+    console.log("Binding navigation data...");
+
+    $("#navigation").html(template(data));
+}
+
+function bindAsideData() {
+    var source   = $("#aside-template").html();
+    var template = Handlebars.compile(source);
+
+    console.log("Loading aside.md markdown file...");
+
+    function successfulyLoadedMarkdownFile(markdownData) {
+        var asideData = metaMarked(markdownData);
+
+        data.activity.aside = asideData.html;
+
+        console.log("Binding aside data...");
+
+        $(".aside").html(template(data));
+    }
+
+    $.ajax({
+            url: "data/aside.md",
+            type: 'get',
+            dataType: 'html',
+            success: successfulyLoadedMarkdownFile
+    });
+
+}
+
+function bindActivityData() {
+    var source   = $("#activity-template").html();
+    var template = Handlebars.compile(source);
+
+    console.log("Loading activity.md markdown file...");
+
+    function successfulyLoadedMarkdownFile(markdownData) {
+        var activityData = metaMarked(markdownData);
+        data.activity.activity = activityData.html;
+        data.activity.meta = activityData.meta;
+
+        console.log("Binding activity data...");
+        console.log("meta", activityData.meta);
+
+        document.title = "Activity: " + activityData.meta.title;
+
+        $(".main").html(template(data));
+
+        // Fix some CSS stuff because markdown can't handle paragraph classes
+
+        $(".activity-instructions > p:nth-child(4)").addClass("lead");
+        $(".activity-instructions > p:nth-child(5)").addClass("leadTime");
+
+        // Execute other bindings
+        bindNavigationData();
+        bindAsideData();
+        bindFooterData();
+
+    }
+
+    $.ajax({
+        url: "data/activity.md",
+        type: 'get',
+        dataType: 'html',
+        success: successfulyLoadedMarkdownFile
+    });
+
+}
+
+function bindFooterData() {
+    var source   = $("#footer-template").html();
+    var template = Handlebars.compile(source);
+
+    console.log("Binding footer data...");
+
+    $(".footer").html(template(data));
+}
+
+// Handlebar helpers
+Handlebars.registerHelper('competencyButton', function(competency) {
+
+    var source   = $("#competency-button-template").html();
+    var template = Handlebars.compile(source);
+    var data = {competency: competency};
+
+    console.log("competency: ", competency);
+
+   return template(data);
+});
+
+
+// Load everything
+
+bindActivityData();
---
+++ data/activity.md
@@ -0,0 +1,30 @@
+---
+title: Kraken the Code
+github: mozilla/webmaker-curriculum/blob/gh-pages/WebLiteracyBasics-I/session01-kraken.html
+image: square-kraken.png
+competencies:
+    - Search
+    - Credibility
+    - Test
+---
+
+## [Section 1. Reading the Web](http://mozilla.github.io/webmaker-curriculum/WebLiteracyBasics-I/)
+
+### Kraken the Code
+
+Made by [MOUSE](http://mouse.org/) and remixed for Clubs by [Mozilla](https://webmaker.org/mentor).
+
+Learners will use the Internet to solve the mystery of The Kraken, a legendary sea creature, while also **learning about search terms, keywords, and how to assess the validity and relevance of web sources.**
+
+45 minutes to an hour
+                    
+### Preparation
+**Complete the** [pre-activity questionnaire](http://goo.gl/forms/Uua6yKIy5E).
+
+Next **do the activity on your own** to become familiar with it.
+
+**Print out the** [Legit-O-Meter Worksheets](https://docs.google.com/a/zythepsary.com/file/d/0B1vyNnSVEMIDbDVLX1E4ZXRmclE/edit).
+
+**Label three pieces of paper** with *“Real,” “Imaginary” and “Something In-between,”* and hang them on different sides of the room.
+
+---
